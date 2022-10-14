6322424a92db9b6a6bf1e566e1e6cf6557c39cb5
Veagle234 <115721730+Veagle234@users.noreply.github.com>
2022-10-14 17:01:01 +0700

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = '💚';
 const giftUrl = '';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Gửi 💚 200%';
-const contentCard = 'Mặc dù tặng không đúng ngày, nhưng mà vẫn chúc em yêu 20-10 hạnh phúc, mãi mãi vui vẻ khi ở bên anh nhé 💋💋💋';
+const contentCard = 'Mặc dù tặng không đúng ngày, nhưng mà vẫn chúc bé 20-10 hạnh phúc, mãi mãi vui vẻ khi ở bên anh nhé 💋💋💋';
 
 
 const giftImage = 'anh.jpg';
